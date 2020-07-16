bcc2ebe15c2bc7c612d2b932a7bf934239147a68
Julius Park <juliusgeo@gmail.com>
2020-07-16 14:56:57 -0400

    prashant's changes fixed

--- pymongoexplain/commands.py
+++ pymongoexplain/commands.py
@@ -20,39 +20,21 @@ from typing import Union
 
 from bson.son import SON
 from pymongo.collection import Collection
+from .utils import convert_to_camelcase
+
 
 Document = Union[dict, SON]
 
+
 class BaseCommand():
-    def __init__(self, command_name, collection):
+    def __init__(self, collection):
         self.command_document = {}
-        self.command_name = command_name
         self.collection = collection
 
-    def convert_to_camelcase(self, d, exclude_keys=[]):
-        if not isinstance(d, dict):
-            return d
-        ret = dict()
-        for key in d.keys():
-            if d[key] is None:
-                continue
-            if key in exclude_keys:
-                ret[key] = d[key]
-                continue
-            new_key = key
-            if "_" in key and key[0] != "_":
-                new_key = key.split("_")[0] + ''.join(
-                    [i.capitalize() for i in key.split("_")[1:]])
-            if isinstance(d[key], list):
-                ret[new_key] = [self.convert_to_camelcase(i,
-                                                          exclude_keys=exclude_keys)
-                                for i in d[key]]
-            elif isinstance(d[key], dict):
-                ret[new_key] = self.convert_to_camelcase(d[key],
-                                                         exclude_keys=exclude_keys)
-            else:
-                ret[new_key] = d[key]
-        return ret
+    @property
+    def command_name(self):
+        """New command classes will specify the command name here."""
+        return None
 
     def get_SON(self):
         cmd = SON([(self.command_name, self.collection)])
@@ -63,7 +45,7 @@ class BaseCommand():
 class UpdateCommand(BaseCommand):
     def __init__(self, collection: Collection, filter, update,
                  kwargs):
-        super().__init__("update", collection.name)
+        super().__init__(collection.name)
         return_document = {"updates":[{"q": filter, "u": update}]}
         for key in kwargs:
             value = kwargs[key]
@@ -71,67 +53,90 @@ class UpdateCommand(BaseCommand):
                 return_document[key] = value
             else:
                 return_document["updates"][0][key] = value
-        self.command_document = self.convert_to_camelcase(return_document)
+        self.command_document = convert_to_camelcase(return_document)
+
+    @property
+    def command_name(self):
+        return "update"
 
 
 class DistinctCommand(BaseCommand):
     def __init__(self, collection: Collection, key, filter, session,
                  kwargs):
-        super().__init__("distinct", collection.name)
+        super().__init__(collection.name)
         self.command_document = {"key": key, "query": filter}
         for key, value in kwargs.items():
             self.command_document[key] = value
-        self.command_document = self.convert_to_camelcase(self.command_document)
+        self.command_document = convert_to_camelcase(self.command_document)
 
+    @property
+    def command_name(self):
+        return "distinct"
 
 class AggregateCommand(BaseCommand):
     def __init__(self, collection: Collection, pipeline, session,
                  cursor_options,
                  kwargs, exclude_keys = []):
-        super().__init__("aggregate", collection.name)
+        super().__init__(collection.name)
         self.command_document = {"pipeline": pipeline, "cursor": cursor_options}
         for key, value in kwargs.items():
             self.command_document[key] = value
 
-        self.command_document = self.convert_to_camelcase(
+        self.command_document = convert_to_camelcase(
             self.command_document, exclude_keys=exclude_keys)
 
+    @property
+    def command_name(self):
+        return "aggregate"
 
 class CountCommand(BaseCommand):
     def __init__(self, collection: Collection, filter,
                  kwargs):
-        super().__init__("count", collection.name)
+        super().__init__(collection.name)
         self.command_document = {"query": filter}
         for key, value in kwargs.items():
             self.command_document[key] = value
-        self.command_document = self.convert_to_camelcase(self.command_document)
+        self.command_document = convert_to_camelcase(self.command_document)
 
+    @property
+    def command_name(self):
+        return "count"
 
 
 class FindCommand(BaseCommand):
     def __init__(self, collection: Collection,
                  kwargs):
-        super().__init__("find", collection.name)
+        super().__init__(collection.name)
         for key, value in kwargs.items():
             self.command_document[key] = value
-        self.command_document = self.convert_to_camelcase(self.command_document)
+        self.command_document = convert_to_camelcase(self.command_document)
 
+    @property
+    def command_name(self):
+        return "find"
 
 class FindAndModifyCommand(BaseCommand):
     def __init__(self, collection: Collection,
                  kwargs):
-        super().__init__("findAndModify", collection.name)
+        super().__init__(collection.name)
         for key, value in kwargs.items():
             self.command_document[key] = value
-        self.command_document = self.convert_to_camelcase(self.command_document)
+        self.command_document = convert_to_camelcase(self.command_document)
+
+    @property
+    def command_name(self):
+        return "findAndModify"
+
 
 class DeleteCommand(BaseCommand):
     def __init__(self, collection: Collection, filter,
                  limit, collation, kwargs):
-        super().__init__("delete", collection.name)
+        super().__init__(collection.name)
         self.command_document = {"deletes": [SON({"q": filter, "limit": limit})]}
         for key, value in kwargs.items():
             self.command_document[key] = value
-        self.command_document = self.convert_to_camelcase(self.command_document)
+        self.command_document = convert_to_camelcase(self.command_document)
 
-
+    @property
+    def command_name(self):
+        return "delete"
