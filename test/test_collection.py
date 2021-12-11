a7cf1b46aa69ff905e0b271c79bcca3b945b545f
julius <juliusgeo@gmail.com>
2021-12-10 21:05:01 -0800

    change to 5.1

--- test/test_collection.py
+++ test/test_collection.py
@@ -245,7 +245,7 @@ class TestExplainableCollection(unittest.TestCase):
         self.assertTrue(self._recursiveIn("allPlansExecution", res))
 
     def test_comment(self):
-        if self.server_version[0] < 5:
+        if tuple(self.server_version) < (5, 1, 0, 0):
             self.skipTest("MongoDB 4.x does not embed the comment in the "
                           "explain document")
         res = self.explain.find({})
