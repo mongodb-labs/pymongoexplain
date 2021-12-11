f47e9e6da22becf437956e3a4da30d144672c2d6
julius <juliusgeo@gmail.com>
2021-12-10 18:09:05 -0800

    add print statement

--- test/test_collection.py
+++ test/test_collection.py
@@ -243,10 +243,12 @@ class TestExplainableCollection(unittest.TestCase):
         self.assertTrue(self._recursiveIn("executionStats", res))
         self.assertTrue(self._recursiveIn("allPlansExecution", res))
 
+        self.explain.update_settings(verbosity=None)
         res = self.explain.find({})
         self.assertFalse(self._recursiveIn("comment", res))
         self.explain.update_settings(comment="hey, I'm a comment")
         res = self.explain.find({})
+        print(res)
         self.assertTrue(self._recursiveIn("comment", res))
 
 
